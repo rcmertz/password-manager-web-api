50839726ce3137e99d8720dc04f80af0081983a9
rcmertz <rcliberalli@hotmail.com>
2022-10-06 17:17:19 -0300

    PassManagerWeb

---
+++ src/main/java/br/com/passmanager/PassManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.passmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/passmanager/PassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
